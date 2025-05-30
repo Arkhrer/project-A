2d393f574f387943015cd94741d9d825d25554cd
Arkhrer <arkhrer@tutamail.com>
2025-05-30 14:32:11 -0300

    Dialogue system bugfixes

--- Scenes/PauseMenu/Scripts/pause_menu.gd
+++ Scenes/PauseMenu/Scripts/pause_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 	pass
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") and Dialogic.current_timeline == null:
 		visible = not visible
 		get_tree().paused = not get_tree().paused
 
